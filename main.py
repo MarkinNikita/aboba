8ff68243c4803788f0dc09e6b5f78ab5a0b0c85e
MarkinNikita <90678947+MarkinNikita@users.noreply.github.com>
2022-11-20 07:28:30 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+
+ 
+server_address = ("", 8000)
+httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
+httpd.serve_forever()
+
+
+
