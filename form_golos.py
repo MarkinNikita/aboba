984f94694f208872de7069c89637fcb22315d008
MarkinNikita <fgh.nikita1@gmail.com>
2022-11-22 15:25:06 +0300

    a

---
+++ form_golos.py
@@ -0,0 +1,15 @@
+import codecs
+
+
+file_golos=codecs.open("test","r","utf-16")
+
+file_golos
+
+
+
+
+
+file_golos.close()
+
+
+
---
+++ form_res.py
@@ -0,0 +1,10 @@
+import codecs
+
+
+
+file_golos=codecs.open("golos","r","utf-16")
+
+
+
+
+file_golos.clsoe()
